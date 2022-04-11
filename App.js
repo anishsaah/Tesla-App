3d9f8d19820c59423ba6117b9df28b9c5fb247a5
Anishsaah <anishsaah@gmail.com>
2022-04-12 04:43:03 +0545

    [Added] Project Tesla App

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( 
+      <View style = {styles.container}>
+          
+          <View style={styles.carContainer}>
+
+              <ImageBackground source={require('./assets/images/ModelX.jpeg')}
+              style={styles.image}/>
+
+              <View style={styles.titles}>
+                  <Text style={styles.title}>Model S</Text>
+                  <Text style={styles.subtitle}>Starting at $69,000</Text>
+                  
+              </View>
+
+          </View>
+
+
+
+
+        <StatusBar style="auto" />    
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    carContainer:{
+        width: '100%',
+        height: '100%',
+
+    },
+
+    titles:{
+        marginTop: '30%',
+        width: '100%',
+        alignItems:'center',
+    },
+
+    title:{
+        fontSize: 40,
+        fontWeight: '500', 
+    },
+
+    subtitle:{
+        fontSize: 16,
+        color: '#5c5e62',
+    },
+
+    image: {
+        width: '100%',
+        height:'100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+    }
+
 });
+
